92e208e9a68054338041c128485436de188a9c4f
Jimmy Tran <jimmytatr21@gmail.com>
2025-01-21 10:10:04 -0500

    Fix class initialization

--- TARS_Runner.py
+++ TARS_Runner.py
@@ -2,6 +2,7 @@ from TARS_Servo_Abstractor import TARS_Servo_Abstractor
 from TARS_Speech import TARS_Speech
 from threading import Thread
 import console
+
 class TARS_Runner:
     def __init__(self):
         self.abstractor = TARS_Servo_Abstractor()
@@ -51,13 +52,13 @@ class TARS_Runner:
                 self.handle_action(self.queue)
 
 def main():
-    console = console.Console()
-    thread = Thread(target=console.bootup_animation)
+    main_console = console.Console()
+    thread = Thread(target=main_console.bootup_animation)
     thread.start()
     TARS = TARS_Runner()
     load_stat = True
     thread.join()
-    console.clear_screen()
+    main_console.clear_screen()
     TARS.start()
 
 if __name__ == "__main__":
